b4b23198e70a3e2f613742e3aa871ec518968efb
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-24 09:42:34 +0530

    feat: create basic express app

---
+++ backend/src/checkEnv.ts
@@ -0,0 +1,1 @@
+// import i from "tiny-invariant"
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,22 @@
-console.log("Hello, world!")
+if (process.env.NODE_ENV !== "production") require("dotenv/config")
+
+import "./checkEnv"
+
+import express from "express"
+import cors from "cors"
+import morgan from "morgan"
+import { createServer } from "http"
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+app.get("/", (_, res) => res.json({ message: "Hello World" }))
+
+const server = createServer(app)
+
+const { PORT = 5000 } = process.env
+server.listen(PORT, () => {
+	console.log("Server listening on port " + PORT)
+})
