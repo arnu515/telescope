ffb3cdeb91f6de438f49d3dfe220d1e637e9aa6c
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-25 12:30:25 +0530

    feat: require redis vars

--- backend/src/checkEnv.ts
+++ backend/src/checkEnv.ts
@@ -1,8 +1,12 @@
 import i from "@arnu515/tiny-invariant"
 
 const { env } = process
+console.log(env)
 
 i(typeof env.DATABASE_URL !== "string", "DATABASE_URL is required")
+
+i(typeof env.REDIS_HOST !== "string", "REDIS_HOST is required")
+i(typeof env.REDIS_PORT !== "string", "REDIS_PORT is required")
 
 i(typeof env.GITHUB_CLIENT_ID !== "string", "GITHUB_CLIENT_ID is required")
 i(
