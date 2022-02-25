423fedaf959929792b3222db5a724a0edabaa0a5
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-25 12:31:42 +0530

    fix: oops

--- backend/src/checkEnv.ts
+++ backend/src/checkEnv.ts
@@ -1,15 +1,14 @@
 import i from "@arnu515/tiny-invariant"
 
 const { env } = process
-console.log(env)
 
-i(typeof env.DATABASE_URL !== "string", "DATABASE_URL is required")
+i(typeof env.DATABASE_URL === "string", "DATABASE_URL is required")
 
-i(typeof env.REDIS_HOST !== "string", "REDIS_HOST is required")
-i(typeof env.REDIS_PORT !== "string", "REDIS_PORT is required")
+i(typeof env.REDIS_HOST === "string", "REDIS_HOST is required")
+i(typeof env.REDIS_PORT === "string", "REDIS_PORT is required")
 
-i(typeof env.GITHUB_CLIENT_ID !== "string", "GITHUB_CLIENT_ID is required")
+i(typeof env.GITHUB_CLIENT_ID === "string", "GITHUB_CLIENT_ID is required")
 i(
-	typeof env.GITHUB_CLIENT_SECRET !== "string",
+	typeof env.GITHUB_CLIENT_SECRET === "string",
 	"GITHUB_CLIENT_SECRET is required"
 )
