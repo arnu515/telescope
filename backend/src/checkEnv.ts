2015eb7691b6ff5270832f53f421c2c90dad0389
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-25 12:18:35 +0530

    fix: use `typeof` checks instead of `!!`

--- backend/src/checkEnv.ts
+++ backend/src/checkEnv.ts
@@ -2,7 +2,10 @@ import i from "@arnu515/tiny-invariant"
 
 const { env } = process
 
-i(!!env.DATABASE_URL, "DATABASE_URL is required")
+i(typeof env.DATABASE_URL !== "string", "DATABASE_URL is required")
 
-i(!!env.GITHUB_CLIENT_ID, "GITHUB_CLIENT_ID is required")
-i(!!env.GITHUB_CLIENT_SECRET, "GITHUB_CLIENT_SECRET is required")
+i(typeof env.GITHUB_CLIENT_ID !== "string", "GITHUB_CLIENT_ID is required")
+i(
+	typeof env.GITHUB_CLIENT_SECRET !== "string",
+	"GITHUB_CLIENT_SECRET is required"
+)
