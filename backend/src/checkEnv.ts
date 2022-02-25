ee956e03e30df4a7089e1f18c5283f22ac194db2
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-25 11:55:42 +0530

    feat: require github credentials

--- backend/src/checkEnv.ts
+++ backend/src/checkEnv.ts
@@ -3,3 +3,6 @@ import i from "@arnu515/tiny-invariant"
 const { env } = process
 
 i(!!env.DATABASE_URL, "DATABASE_URL is required")
+
+i(!!env.GITHUB_CLIENT_ID, "GITHUB_CLIENT_ID is required")
+i(!!env.GITHUB_CLIENT_SECRET, "GITHUB_CLIENT_SECRET is required")
