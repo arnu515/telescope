d63a0f447f8343308d95b936b2c69856c5713391
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-25 09:00:56 +0530

    feat: separate the express app into another file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,8 @@
 if (process.env.NODE_ENV !== "production") require("dotenv/config")
 
 import "./checkEnv"
-
-import express from "express"
-import cors from "cors"
-import morgan from "morgan"
+import app from "./app"
 import { createServer } from "http"
-
-const app = express()
-app.use(express.json())
-app.use(cors())
-app.use(morgan("dev"))
-
-app.get("/", (_, res) => res.json({ message: "Hello World" }))
 
 const server = createServer(app)
 
