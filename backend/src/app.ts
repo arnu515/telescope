bcd4300cb4504c7ef35e37d6787687544f050420
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-25 12:57:24 +0530

    feat: support for templates

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
 import express from "express"
 import cors from "cors"
 import morgan from "morgan"
+import path from "path"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(morgan("dev"))
+
+app.set("view engine", "ejs")
+app.set("views", path.join(__dirname, "templates"))
+app.use(express.static(path.join(__dirname, "static")))
 
 import routers from "./routers"
 
