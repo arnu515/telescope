a8840c8de008d8044aecc7a82e2756a3e2494a89
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-02-27 11:30:38 +0530

    feat: method for adding integration

--- backend/src/routers/integrations.ts
+++ backend/src/routers/integrations.ts
@@ -21,13 +21,29 @@ router.get("/public/:id", async (req, res) => {
 		}
 	})
 	if (!integration) {
-		res
-			.status(404)
-			.json({
-				error: "Integration not found",
-				error_description: "This integration could not be found"
-			})
+		res.status(404).json({
+			error: "Integration not found",
+			error_description: "This integration could not be found"
+		})
 	} else res.json({ integration })
+})
+
+router.get("/public/:id/add", async (req, res) => {
+	const { id } = req.params
+	const integration = await prisma.integration.findFirst({
+		where: {
+			id,
+			isVerified: true
+		}
+	})
+	if (!integration) {
+		res.status(404).render("error", {
+			status: 404,
+			error: "Integration not found",
+			error_description: "This integration could not be found",
+			frontend: process.env.SELF_URL || "http://localhost:3000"
+		})
+	} else res.redirect(integration.addUrl)
 })
 
 export default router
