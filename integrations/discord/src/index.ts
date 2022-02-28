b05b9fe14af395e7c80d65466da08816325ea8fc
arnu515 <arnu515@protonmail.com>
2022-02-28 10:43:59 +0000

    feat: auth

--- integrations/discord/src/index.ts
+++ integrations/discord/src/index.ts
@@ -1,4 +1,5 @@
 import { serve } from "https://deno.land/std@0.127.0/http/server.ts";
+import { customAlphabet } from "https://cdn.skypack.dev/nanoid";
 import { except, i } from "./lib/invariant.ts";
 import slash from "./lib/slash.ts";
 import telescope, { TelescopeError } from "./lib/telescope.ts";
@@ -16,8 +17,7 @@ function json(data: any, init?: ResponseInit) {
   return new Response(JSON.stringify(data), init);
 }
 
-const handler = async (req: Request) => {
-  // Request should be post and have correct headers
+const discordInteractionHandler = async (req: Request) => {
   if (req.method.toLowerCase() !== "post") {
     return json({
       error: "Method not allowed",
@@ -167,11 +167,186 @@ const callCommand = async (body: Record<string, any>) => {
   }
 };
 
+const generateState = customAlphabet("0123456789abcdef", 16);
+const states: string[] = [];
+
+const authHandler = async (req: Request) => {
+  if (req.method !== "GET") {
+    return json({
+      error: "Method not allowed",
+      error_description: "Please send a GET request",
+    }, { status: 405 });
+  }
+
+  const url = new URL(req.url);
+  const key = url.searchParams.get("key");
+  const code = url.searchParams.get("code");
+  const state = url.searchParams.get("state");
+  const callId = url.searchParams.get("call_id");
+  const err1 = await except(
+    () => {
+      if (typeof key === "string") {
+        i(key === telescope.opts.key, "Invalid key");
+        i(typeof callId === "string", "call_id not present in query string");
+      }
+      if (typeof code === "string") {
+        i(typeof state === "string", "state not present in query string");
+        i(!!states.find((i) => i === state), "Invalid state");
+        i(state!.split("-").length === 2, "Invalid state");
+      }
+    },
+  );
+
+  if (err1) {
+    return json({
+      error: "Invalid request",
+      error_description: err1,
+    }, { status: 422 });
+  }
+
+  if (typeof key === "string") {
+    // need to redirect to discord for OAuth
+    let call;
+    try {
+      call = await telescope.getCall(callId!);
+    } catch (e) {
+      console.log(e, e.body);
+      return json(e.body, { status: e.status });
+    }
+
+    const state = call.id + "-" + generateState();
+    states.push(state);
+
+    return new Response(undefined, {
+      headers: {
+        location: `https://discord.com/api/oauth2/authorize?client_id=${
+          encodeURIComponent(Deno.env.get("DISCORD_CLIENT_ID")!)
+        }&redirect_uri=${
+          encodeURIComponent(Deno.env.get("DISCORD_REDIRECT_URL")!)
+        }&state=${encodeURIComponent(state)}&response_type=code&scope=identify`,
+      },
+      status: 302,
+    });
+  } else if (typeof code === "string") {
+    // back from discord
+    const callId = state!.split("-")[0];
+    let call;
+    try {
+      call = await telescope.getCall(callId);
+    } catch (e) {
+      console.log(e, e.body);
+      return json(e.body, { status: e.status });
+    }
+    states.splice(states.indexOf(state!), 1);
+
+    // deno-lint-ignore no-inner-declarations
+    async function forwardError(
+      data: { error: string; error_description: string },
+    ) {
+      const url = await telescope.getForwardErrorUrl(callId, data);
+      console.log(url);
+      return new Response(undefined, {
+        headers: {
+          location: url,
+        },
+        status: 302,
+      });
+    }
+
+    const body = new URLSearchParams({
+      client_id: Deno.env.get("DISCORD_CLIENT_ID")!,
+      client_secret: Deno.env.get("DISCORD_CLIENT_SECRET")!,
+      grant_type: "authorization_code",
+      code: code,
+      redirect_uri: Deno.env.get("DISCORD_REDIRECT_URL")!,
+    });
+    const tokenRes = await fetch("https://discord.com/api/v9/oauth2/token", {
+      method: "POST",
+      body,
+    });
+    const tokenData = await tokenRes.json();
+    if (tokenRes.status !== 200) {
+      return forwardError({
+        error: "Authentication failed",
+        error_description: "Failed to get token",
+      });
+    }
+    const { access_token, scope } = tokenData;
+    console.log(scope);
+    if (!scope.includes("identify")) {
+      return forwardError({
+        error: "Invalid scope",
+        error_description:
+          "This request has been tampered with. Please try again",
+      });
+    }
+    const userRes = await fetch("https://discord.com/api/users/@me", {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    const userData = await userRes.json();
+    if (userRes.status !== 200) {
+      console.error(userData);
+      return forwardError({
+        error: "Authentication failed",
+        error_description: "Failed to get user info",
+      });
+    }
+    const discordUser = userData;
+    await fetch("https://discord.com/api/oauth/token/revoke", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+    if (call.toId !== discordUser.id && call.fromId !== discordUser.id) {
+      return forwardError({
+        error: "Authentication failed",
+        error_description: "You are not allowed to access this call",
+      });
+    }
+
+    const authUrl = await telescope.getAuthUrl(call.id, {
+      nickname: discordUser.username,
+      avatarUrl: discordUser.avatar
+        ? `https://cdn.discordapp.com/avatars/${discordUser.id}/${discordUser.avatar}.png`
+        : `https://cdn.discordapp.com/embed/avatars/${
+          discordUser.discriminator % 5
+        }.png`,
+    });
+
+    return new Response(undefined, {
+      headers: {
+        location: authUrl,
+      },
+      status: 302,
+    });
+  } else {
+    return json({
+      error: "Invalid request",
+      error_description: "Invalid request",
+    }, { status: 422 });
+  }
+};
+
 console.log("Starting server on port: 8000");
 serve(async (req) => {
   // console.log(req);
   const time = Date.now();
-  const res = await handler(req);
+  const url = new URL(req.url);
+  let res;
+  if (url.pathname === "/interaction") {
+    res = await discordInteractionHandler(req);
+  } else if (url.pathname === "/") {
+    res = json({ message: "Pong" });
+  } else if (url.pathname === "/auth") {
+    res = await authHandler(req);
+  } else {
+    res = json({ error: "Not found", error_description: "Route not found" }, {
+      status: 404,
+    });
+  }
   // console.log(res);
   console.log("Time taken: " + (Date.now() - time));
   return res;
