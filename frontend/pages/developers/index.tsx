dd91d211794cd1f1f27ef1596f0c440291007455
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-02 16:40:20 +0530

    feat: display user's integrations

--- frontend/pages/developers/index.tsx
+++ frontend/pages/developers/index.tsx
@@ -1,17 +1,36 @@
 import { GetServerSideProps } from 'next'
+import Link from 'next/link'
 import React from 'react'
+import IntegrationCard from '../../lib/components/IntegrationCard'
 import { getSession } from '../../lib/session'
-import { Developer } from '../../lib/util/types'
+import api from '../../lib/util/api'
+import { Developer, Integration } from '../../lib/util/types'
 
 export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
   const session = await getSession(req, res)
   if (!session.dev)
     return { redirect: { statusCode: 302, destination: '/developers/auth' } }
 
-  return { props: { dev: session.dev ?? null } }
+  try {
+    const integrations =
+      (await api.developers.integrations.all(session.token)).data
+        ?.integrations ?? null
+    return { props: { dev: session.dev ?? null, integrations } }
+  } catch (e) {
+    console.log(e)
+    session.dev = null
+    session.token = null
+    return { redirect: { statusCode: 302, destination: '/developers/auth' } }
+  }
 }
 
-const DevelopersIndex: React.FC<{ dev: Developer }> = ({ dev }) => {
+const DevelopersIndex: React.FC<{
+  dev: Developer
+  integrations: Integration[]
+}> = ({ dev, integrations }) => {
+  if (!dev) return null
+  if (!integrations) return null
+
   return (
     <React.Fragment>
       <div className="mx-auto max-w-screen-lg py-12">
@@ -34,6 +53,19 @@ const DevelopersIndex: React.FC<{ dev: Developer }> = ({ dev }) => {
             Logout
           </a>
         </p>
+        <p className="mt-6 mb-12 text-center">
+          <Link href="/">
+            <a className="button mr-4 bg-success">Homepage</a>
+          </Link>
+        </p>
+        <h2 className="mt-12 text-center text-3xl font-medium">
+          Your integrations
+        </h2>
+        <div className="my-4 mx-auto flex max-w-screen-lg flex-col gap-4 py-12">
+          {integrations.map((x, y) => (
+            <IntegrationCard integration={x} key={y} />
+          ))}
+        </div>
       </div>
     </React.Fragment>
   )
