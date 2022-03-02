709cb799c26f64ba5d0230957664e3bd7f179ca1
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-02 16:53:05 +0530

    feat: add

---
+++ frontend/pages/developers/integrations/index.tsx
@@ -0,0 +1,12 @@
+import { GetServerSideProps } from 'next'
+import React from 'react'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  return { redirect: { statusCode: 302, destination: '/developers' } }
+}
+
+const DevelopersIntegrations: React.FC = () => {
+  return <h1>Redirecting...</h1>
+}
+
+export default DevelopersIntegrations
