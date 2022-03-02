b1cb80235daaeefb45085c22b5c7ba4772fb7696
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-02 13:52:42 +0530

    feat: start work on index page

---
+++ frontend/pages/developers/index.tsx
@@ -0,0 +1,42 @@
+import { GetServerSideProps } from 'next'
+import React from 'react'
+import { getSession } from '../../lib/session'
+import { Developer } from '../../lib/util/types'
+
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+  const session = await getSession(req, res)
+  if (!session.dev)
+    return { redirect: { statusCode: 302, destination: '/developers/auth' } }
+
+  return { props: { dev: session.dev } }
+}
+
+const DevelopersIndex: React.FC<{ dev: Developer }> = ({ dev }) => {
+  return (
+    <React.Fragment>
+      <div className="mx-auto max-w-screen-lg py-12">
+        <h1 className="my-4 text-center text-5xl font-bold">
+          Hello, {dev.name}!
+        </h1>
+        <p className="mt-12 text-center font-medium">
+          Signed in as{' '}
+          <a
+            href={`https://github.com/${dev.username}`}
+            className="text-success hover:underline"
+          >
+            {dev.username}
+          </a>
+          .{' '}
+          <a
+            href="/api/developers/logout"
+            className="text-success hover:underline"
+          >
+            Logout
+          </a>
+        </p>
+      </div>
+    </React.Fragment>
+  )
+}
+
+export default DevelopersIndex
