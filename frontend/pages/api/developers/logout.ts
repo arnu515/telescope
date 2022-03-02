65416455e568bdfbda5dd285e76467aa81424d53
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-02 13:52:31 +0530

    feat: add

---
+++ frontend/pages/api/developers/logout.ts
@@ -0,0 +1,13 @@
+import { NextApiHandler } from 'next'
+import { getSession } from '../../../lib/session'
+
+const handler: NextApiHandler = async (req, res) => {
+  const session = await getSession(req, res)
+
+  session.dev = null
+
+  if (req.method === 'GET') res.redirect('/developers/auth')
+  else res.status(200).json({})
+}
+
+export default handler
