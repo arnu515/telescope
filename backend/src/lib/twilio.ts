11eec5b961c16b342bf64bc2c0b8d3532308f615
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-06 14:57:00 +0530

    feat: extract env vars to separate variables

--- backend/src/lib/twilio.ts
+++ backend/src/lib/twilio.ts
@@ -1,9 +1,9 @@
 import Twilio from "twilio"
 
-const twilio = Twilio(
-	process.env.TWILIO_API_SID,
-	process.env.TWILIO_API_SECRET,
-	{ accountSid: process.env.TWILIO_ACCOUNT_SID }
-)
+export const apiSid = process.env.TWILIO_API_SID!
+export const apiSecret = process.env.TWILIO_API_SECRET!
+export const accountSid = process.env.TWILIO_ACCOUNT_SID!
+
+const twilio = Twilio(apiSid, apiSecret, { accountSid })
 
 export default twilio
