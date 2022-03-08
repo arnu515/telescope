acd4aa947213f0b9239127f3061c80d718180963
arnu515 <52203828+arnu515@users.noreply.github.com>
2022-03-08 22:30:33 +0530

    fix: update link for telescope sdk

--- integrations/discord/src/lib/telescope.ts
+++ integrations/discord/src/lib/telescope.ts
@@ -1,5 +1,5 @@
-import Telescope from "https://raw.githubusercontent.com/arnu515/telescope/calls-and-integrations/integrations/sdk-deno/mod.ts";
-export { TelescopeError } from "https://raw.githubusercontent.com/arnu515/telescope/calls-and-integrations/integrations/sdk-deno/src/index.ts";
+import Telescope from "https://raw.githubusercontent.com/arnu515/telescope/master/integrations/sdk-deno/mod.ts";
+export { TelescopeError } from "https://raw.githubusercontent.com/arnu515/telescope/master/integrations/sdk-deno/src/index.ts";
 
 const telescope = new Telescope({
   clientId: Deno.env.get("TELESCOPE_CLIENT_ID")!,
